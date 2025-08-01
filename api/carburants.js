d62e4dcb384b59b41f8a49f1504b43a7f1034c83
adubus <106705517+adubus@users.noreply.github.com>
2025-08-02 00:41:54 +0200

    Update carburants.js

--- api/carburants.js
+++ api/carburants.js
@@ -11,9 +11,10 @@ const response = await fetch(xmlUrl, { agent: new https.Agent({ rejectUnauthoriz
     res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate");
     res.status(200).json(stations);
   } catch (error) {
-    console.error("Erreur récupération carburants:", error);
-    res.status(500).json({ error: "Erreur récupération des données" });
-  }
+  console.error("Erreur récupération carburants:", error);
+  res.status(500).json({ error: error.message || "Erreur récupération des données" });
+}
+  
 }
 
 async function parseXmlToStations(xml) {
